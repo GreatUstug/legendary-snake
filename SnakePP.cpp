4780dd3cdc4403eae7ea3abeb7ddb8fdcd5b80f7
GreatUstug <m.ustugoff@gmail.com>
2025-10-19 21:49:00 +0300

    solid snake

--- SnakePP.cpp
+++ SnakePP.cpp
@@ -10,8 +10,8 @@
 #include <random>
 
 //размерности поля
-const int ROWS = 5;
-const int COLS = 5;
+const int ROWS = 10;
+const int COLS = 10;
 const int ROWS_WITH_BORDERS = ROWS + 2;
 const int COLS_WITH_BORDERS = COLS + 2;
 //спец символы
@@ -29,7 +29,7 @@ const char DOWN = 's';
 const char RIGHT = 'd';
 const char QUIT = 'q';
 //время движения
-const auto MOVE_INTERVAL = std::chrono::milliseconds(500);
+const auto MOVE_INTERVAL = std::chrono::milliseconds(200);
 // очистка экрана
 const std::string CLEAR_SCREEN = "\033[H";
 //счет для выигрыша
@@ -93,55 +93,59 @@ Snake SpawnSnake() {
     return snake;
 }
 
-//переделать
 void DrawSnakeOnGrid(Grid& grid, const Snake& snake) {
     for (size_t i = 0; i < snake.size(); ++i) {
         const auto& part = snake[i];
-        if (part.y >= 0 && part.y < static_cast<int>(grid.size()) &&
-            part.x >= 0 && part.x < static_cast<int>(grid[SNAKE_HEAD_IND].size())) {
-            grid[part.y][part.x] = part.type;
-        }
+        grid[part.y][part.x] = part.type;
     }
 }
 
 //движение змейки
-void MoveSnake(Snake& snake) {
+Snake MoveSnake(const Snake& snake, const char& direction) {
+    if (snake.empty()) return snake;
     SnakePart head = snake.front();
-    char dir = head.dir;
 
-    switch (dir) {
+    switch (direction) {
         case UP: head.y--; break;
         case DOWN: head.y++; break;
         case LEFT: head.x--; break;
         case RIGHT: head.x++; break;
-        default: return;
+        default: return snake;
     }
 
     head.type = SNAKE_HEAD_CHAR;
-    snake.push_front(head);
+    head.dir = direction;
 
-    if (snake.size() > 1) {
-        snake[1].type = SNAKE_BODY_CHAR;
+    Snake newSnake;
+    newSnake.push_front(head);
+
+    
+    for (size_t i = 1; i < snake.size(); ++i) {
+        SnakePart part = snake[i - 1];
+        part.type = SNAKE_BODY_CHAR;
+        newSnake.push_back(part);
     }
-    snake.pop_back();
+
+    if (newSnake.size() > snake.size()) newSnake.pop_back();
+
+    return newSnake;
 }
 
-bool HandleInput(Snake& snake) {
+char HandleInput(Snake& snake) {
     if (_kbhit()) {
         char key = _getch();
         char currDir = snake[SNAKE_HEAD_IND].dir;
 
         if (key == QUIT) {
-            return true;
+            return QUIT;
         }
 
         if ((key == UP && currDir != DOWN) ||
             (key == DOWN && currDir != UP) ||
             (key == LEFT && currDir != RIGHT) ||
             (key == RIGHT && currDir != LEFT)) {
-            snake[SNAKE_HEAD_IND].dir = key;
+            return key;
         }
-        return false;
     }
 }
 
@@ -157,8 +161,8 @@ bool ValidPosition(const Snake& snake) {
         snakeHead.y <= 0 || snakeHead.y >= ROWS_WITH_BORDERS - 1
         ) return false;
     //проверка на врезание в саму себя
-    for (SnakePart part : snakeBody) {
-        if (part.x == snakeHead.x && part.y == snakeHead.y) return false;
+    for (size_t i = 1; i < snake.size(); i++) {
+        if (snake[i].x == snakeHead.x && snake[i].y == snakeHead.y) return false;
     }
     return true;
 }
@@ -193,8 +197,16 @@ void DrawAppleOnGrid(Grid& grid, const Apple& apple) {
 }
 
 Snake IncreaseSnake(const Snake& snake) {
+    if (snake.empty()) return snake;
     Snake newSnake = snake;
-    newSnake.push_back(snake.back());
+    if (newSnake.size() >= 2) {
+        newSnake.push_back(snake.back());
+    }
+    else {
+        newSnake.push_back(snake.back());
+        newSnake.back().type = SNAKE_BODY_CHAR;
+    }
+
     return newSnake;
 }
 
@@ -206,26 +218,39 @@ void RenderGame() {
     auto lastMoveTime = std::chrono::steady_clock::now();
 
     while (scoreCounter != WIN_SCORE) {
-        if (HandleInput(snake)) {
+        char inputDir = HandleInput(snake);
+
+        if (inputDir == QUIT) {
             std::cout << "PLAYER QUIT GAME" << '\n';
             return;
         }
+
         auto now = std::chrono::steady_clock::now();
+        Snake updatedSnake = snake;
+        Apple updatedApple = apple;
+        int updatedScore = scoreCounter;
+
         if (now - lastMoveTime >= MOVE_INTERVAL) {
-            MoveSnake(snake);
-            if (!ValidPosition(snake)) {
+            updatedSnake = MoveSnake(updatedSnake, inputDir);
+            if (!ValidPosition(updatedSnake)) {
                 std::cout << "GAME OVER" << '\n';
                 return;
             }
             lastMoveTime = now;
         }
         
-        if (IsAppleEaten(snake[SNAKE_HEAD_IND], apple)) 
+        if (IsAppleEaten(updatedSnake[SNAKE_HEAD_IND], updatedApple)) 
         {
-            apple = SpawnApple(snake);
-            snake = IncreaseSnake(snake);
-            scoreCounter++;
+            updatedApple = SpawnApple(updatedSnake);
+            updatedSnake = IncreaseSnake(updatedSnake);
+            updatedScore++;
         }
+
+        snake = updatedSnake;
+        apple = updatedApple;
+        scoreCounter = updatedScore;
+        inputDir = snake[SNAKE_HEAD_IND].dir;
+
         grid = CreateGrid();
         DrawAppleOnGrid(grid, apple);
         DrawSnakeOnGrid(grid, snake);
@@ -239,9 +264,6 @@ void RenderGame() {
 int main() 
 {
     std::cout << "PRESS ANY BUTTON TO START PLAY SNAKE!" << '\n';
-    std::cout << "CHOOSE LEVEL OF DIFICULT:" << '\n';
-    std::cout << "1 - WORM - EASY LEVEL" << '\n';
-    std::cout << "2 - SOLID SNAKE - NORMAL LEVEL" << '\n';
     std::cin.get();
     system("cls");
     RenderGame();
